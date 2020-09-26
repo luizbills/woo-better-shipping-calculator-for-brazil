a2f7ffe1e53371250c0aaa69df4701fda48ff023
Luiz "Bills <luizpbills@gmail.com>
2020-09-26 09:13:18 -0300

    Create .init.sh

---
+++ .init.sh
@@ -0,0 +1,6 @@
+# docs: https://developer.wordpress.org/cli/commands/plugin/
+
+wp plugin delete hello
+wp plugin delete akismet
+wp plugin install woocommerce --activate
+wp plugin activate ${REPO_NAME}

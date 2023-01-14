759c3c2d61d5a9db6d6f93955968384e77d83c96
Luiz Bills <luizbills@pm.me>
2023-01-14 10:14:04 -0300

    fix postcode field

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -2,31 +2,32 @@ window.jQuery(($) => {
     const params = window.wc_better_shipping_calculator_for_brazil_params;
     if (!params) return;
 
-    const refs = {};
-    const body = $(document.body);
-    const placeholder = params.postcode_placeholder;
     const selectors = params.selectors;
-    const input_type = params.postcode_input_type;
+    const refs = {
+        body: $(document.body),
+    };
 
-    body.on('updated_wc_div', update_refs);
-    body.on('country_to_state_changed', update_refs);
     update_refs();
+    refs.body.on('updated_wc_div', update_refs);
+    refs.body.on('country_to_state_changed', update_refs);
 
-    body.on('keyup', selectors.postcode, apply_postcode_mask);
+    refs.body.on('keyup', selectors.postcode, apply_postcode_mask);
 
-    function apply_postcode_mask() {
-        const value = refs.postcode.val() || '';
+    function apply_postcode_mask(evt) {
+        const input = evt ? $(evt.target) : refs.postcode;
+        const value = input.val() || '';
         let postcode = value.replace(/[^0-9]/g, '').slice(0, 8);
         if (postcode.length > 5) {
             postcode = postcode.slice(0, 5) + '-' + postcode.slice(5);
         }
-        refs.postcode.val(postcode);
-        refs.postcode.prop('maxLength', 9); // length of "99999-999"
+        input.val(postcode);
+        input.prop('maxLength', 9); // length of "99999-999"
     }
 
     function update_refs() {
+        const placeholder = params.postcode_placeholder;
+        const input_type = params.postcode_input_type;
         refs.postcode = $(selectors.postcode);
-        // update placeholder and title
         if (placeholder) {
             refs.postcode.prop('placeholder', placeholder);
             refs.postcode.prop('title', placeholder);
